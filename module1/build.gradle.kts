adf083fd92e727d6987a150c4de9cdded814ca69
jorgezwift <jorge.zwift@gmail.com>
2022-11-13 16:12:23 +0000

    a

--- module1/build.gradle.kts
+++ module1/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     kotlin("jvm")
     id("pl.allegro.tech.build.axion-release") version "1.14.0"
 }
+scmVersion {
+    tag {
+        prefix.set("module1")
+    }
+}
 
 project.version = scmVersion.version
 
--- module2/build.gradle.kts
+++ module2/build.gradle.kts
@@ -2,5 +2,9 @@ plugins {
     kotlin("jvm")
     id("pl.allegro.tech.build.axion-release") version "1.14.0"
 }
-
+scmVersion {
+    tag {
+        prefix.set("module2")
+    }
+}
 project.version = scmVersion.version
